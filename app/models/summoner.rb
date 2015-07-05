bd7f5888873617d8ab0b999474cf9ef578fe0a81
Christopher Batty <cmbats@gmail.com>
2015-07-05 12:33:40 -0400

    Working Commit before pushing for MVP

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -22,10 +22,10 @@ class Summoner < ActiveRecord::Base
 				@summoner.save
 			else
 				summId = @responseHash[call.summName]['id']
-				@summoner = Summoner.new(:formattedName => @responseHash[call.summName]['name'], :internalName => call.summName, :summonerId => summId, :lastUpdated => (Time.now.to_i - ENV['UPDATE_AFTER_SECONDS'].to_i) )
+				@summoner = Summoner.new(:formattedName => @responseHash[call.summName]['name'], :internalName => call.summName, :summonerId => summId, :lastUpdated => (Time.now - ENV['UPDATE_AFTER_SECONDS'].to_i) )
 				@summoner.save
 			end
-		elsif (Time.now.to_i - @summoner.lastUpdated > 900)
+		elsif (Time.now.to_i - @summoner.lastUpdated.to_i > 900)
 			call = RiotApiCall.new(:server => server, :summName => @internalName)
 			call.getSummonerByName
 			call.save
@@ -35,7 +35,7 @@ class Summoner < ActiveRecord::Base
 			@summoner.save
 		end
 		
-		if (Time.now.to_i - @summoner.lastUpdated) > 900
+		if (Time.now.to_i - @summoner.lastUpdated.to_i) > 900
 			#If the summoner is due for a game update, update the games.
 			request2 = "https://" + server.downcase + ".api.pvp.net/api/lol/" + server.downcase + "/v2.2/matchhistory/" + @summoner.summonerId.to_s + "?api_key=" + ENV['RIOT_API_KEY'].to_s
 			call = RiotApiCall.new(:server => server.downcase, :api_call => request2, :summName => @internalName)
@@ -44,7 +44,7 @@ class Summoner < ActiveRecord::Base
 			call.save
 			game = Game.new(:gameData => resp, :gameId => resp['matches'][9]['matchId'])			
 			@summoner.lastGameId = game.gameId
-			@summoner.lastUpdated = Time.now.to_i
+			@summoner.lastUpdated = Time.now
 			game.save
 			@summoner.save
 			logger.info "Game #{game.gameId} saved."
