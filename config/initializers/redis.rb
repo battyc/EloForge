c02c9f64608c0c0f7e05a42aef1979f78cd25f8f
Christopher Batty <cmbats@gmail.com>
2015-07-17 00:11:58 -0400

    Added loading icon

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-#$redis = Redis.new(url: ENV["REDIS_URL"]) # PRODUCTION
-$redis = Redis.new(:host => '127.0.0.1', :port => 6379) #DEVELOPMENT
+$redis = Redis.new(url: ENV["REDIS_URL"]) # PRODUCTION
+#$redis = Redis.new(:host => '127.0.0.1', :port => 6379) #DEVELOPMENT
